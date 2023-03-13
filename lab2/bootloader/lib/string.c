c34ac050f03956639026cacbc7539c3fa2644a82
me356500 <xlpenix.cs07@nycu.edu.tw>
2023-03-13 10:19:38 +0800

    2

--- lab2/bootloader/lib/string.c
+++ lab2/bootloader/lib/string.c
@@ -22,3 +22,14 @@ int strcmp(const char *s1, const char *s2) {
     }
     return *s1 - *s2;
 }
+
+int strncmp(const char *s1, const char *s2, int n) {
+    while (n-- && *s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    if (n == (int) -1) {
+        return 0;
+    }
+    return *(const unsigned char *) s1 - *(const unsigned char *) s2;
+}
--- lab2/kernel/include/string.h
+++ lab2/kernel/include/string.h
@@ -1,3 +1,4 @@
 int strlen(const char *s);
 char *strcpy(char *dest, const char *src);
 int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int n);
